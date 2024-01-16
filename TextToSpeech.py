ff19cfea53753a1fd9b174f9cf311baa9630ae85
Rifqi Fachriza S <mysteriza@proton.me>
2024-01-17 02:29:48 +0700

    Refactor code and add Wit.ai integration

--- TextToSpeech.py
+++ TextToSpeech.py
@@ -1,24 +1,33 @@
-import os
 import pygame
+import subprocess
 
 
 def speak(text):
     voice = "id-ID-ArdiNeural"
     output_file = "audio/output.mp3"
 
-    command = (
-        f'edge-tts --voice "{voice}" --text "{text}" --write-media "{output_file}"'
-    )
-
-    os.system(command)
-
-    pygame.init()
-    pygame.mixer.init()
+    # Use subprocess module for a more flexible and secure command execution
+    command = [
+        "edge-tts",
+        "--voice",
+        voice,
+        "--text",
+        text,
+        "--write-media",
+        output_file,
+    ]
 
     try:
+        # Use subprocess.run instead of os.system for better control
+        subprocess.run(command, check=True)
+
+        pygame.init()
+        pygame.mixer.init()
+
         pygame.mixer.music.load(output_file)
         pygame.mixer.music.play()
 
+        # Use pygame.mixer.music.get_busy() directly in the while loop condition
         while pygame.mixer.music.get_busy():
             pygame.time.Clock().tick(10)
 
